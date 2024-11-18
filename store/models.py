e45494592130cbb283189fcdb42026e8df3c5dad
Vijayaraj <vjlingam7@gmail.com>
2024-11-18 15:39:11 +0530

    Added multiple models and establishted a relation

--- store/models.py
+++ store/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 # Create your models here.
-
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
-    
+    collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
+
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
@@ -26,7 +29,6 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1,default=MEMBERSHIP_BRONZE,choices=MEMBERSHIP_CHOICES)
 
-
 class Order(models.Model):
     PAYMENT_CHOICES = [
         ('P',"Pending"),
@@ -35,9 +37,25 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1,choices=PAYMENT_CHOICES,default='P')
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
 
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    
